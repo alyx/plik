eb2a9089673b9656ebd571c3cc12fc06e96f2a15
SkyezerFox <actuallyori@gmail.com>
2022-02-14 01:21:53 +0000

    simplify server health check logic

--- webapp/components/ServerHealth.tsx
+++ webapp/components/ServerHealth.tsx
@@ -1,32 +1,27 @@
 import { useEffect, useState } from "react";
 
-import { InfoIcon, WarningIcon } from "@chakra-ui/icons";
+import { WarningIcon } from "@chakra-ui/icons";
 import { Box, ScaleFade, Tooltip } from "@chakra-ui/react";
 
 import { getVersion } from "../api/server";
 
 export const ServerHealth: React.FC = () => {
-	const [version, setVersion] = useState("");
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
-		(async () => {
-			const version = await getVersion().catch(err => {
-				console.error(err);
-				return null;
-			});
-			setVersion(version);
-		})();
+		getVersion().catch(err => {
+			setError(true);
+		});
 	}, []);
 
 	return (
 		<Box position="absolute" right="0" top="0" padding={5}>
 			<ScaleFade in={true}>
-				{version === null && (
+				{error && (
 					<Tooltip label="There was an error fetching version information from the server. Uploading files may not work correctly!">
 						<WarningIcon height="32px" width="32px" color="#ffaaaa" />
 					</Tooltip>
 				)}
-				{typeof version === "string" && <InfoIcon height="32px" width="32px" />}
 			</ScaleFade>
 		</Box>
 	);
